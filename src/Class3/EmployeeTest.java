a7fd8643c178baa0d6681149ddf6c9962044cb4f
Gigbyte <lparoi@gmail.com>
2023-11-04 21:02:40 +0600

    all java code

--- src/Class3/EmployeeTest.java
+++ src/Class3/EmployeeTest.java
@@ -1,12 +1,12 @@
 package Class3;
 
-import java.util.EnumMap;
+
 
 public class EmployeeTest {
     public static void main(String[] args) {
         Employee employee = new Employee();
         employee.name = "Leemon";
-        employee.id = 9905656l;
+        employee.id = 5656;
         employee.designation = "SQAE";
         employee.salary =352352345.25f;
         System.out.println(employee.getBonus());

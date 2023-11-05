28535e898aea85e0df5408f9ac5b4d251f414a9c
Gigbyte <lparoi@gmail.com>
2023-11-05 21:32:17 +0600

    git Branch create new line has created

--- src/GitTest/LeemonTest.java
+++ src/GitTest/LeemonTest.java
@@ -3,5 +3,6 @@ package GitTest;
 public class LeemonTest {
     public static void main(String[] args) {
         System.out.println("Leemon Paroi");
+        System.out.println("Leemon Paroi");
     }
 }
